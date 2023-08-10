198da7a9bdd257c55e479aba7e30a6e09681c301
Prakash Bist <prakash.bist@smartsolar.com.np>
2023-08-10 11:38:36 +0900

    Initial Commit

---
+++ TwitterX/TwitterXApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterXApp.swift
+//  TwitterX
+//
+//  Created by Prakash Bist on 2023/08/10.
+//
+
+import SwiftUI
+
+@main
+struct TwitterXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
